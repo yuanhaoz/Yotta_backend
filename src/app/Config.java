a274788b040188d5a2db0eb0e2b1a0dde9bc943a
yuanhao <994303>
2017-10-23 10:08:41 +0800

    修改注释

--- src/app/Config.java
+++ src/app/Config.java
@@ -23,9 +23,9 @@ public class Config {
 	/**
 	 * Mysql 配置
 	 */
-	public static String MYSQL_URL = "jdbc:mysql://localhost:3306/yotta_create?user=root&password=root&characterEncoding=UTF8"; // 数据库
-	public static String IP1="http://202.117.54.39"; // 本机：域名+apache端口
-	public static String IP2="http://202.117.54.39:8080/Yotta"; // 本机：域名+http端口
+	public static String MYSQL_URL = "jdbc:mysql://localhost:3306/yotta_create?user=root&password=root&characterEncoding=UTF8"; // 阿里云服务器：域名+http端口
+	public static String IP1="http://202.117.54.39"; // 跨域访问控制：域名+apache端口
+	public static String IP2="http://202.117.54.39:8080/Yotta"; // 阿里云服务器：域名+http端口
 	
 	/**
 	 * 数据库  配置
