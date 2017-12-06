44ed0dad50b478baec7c211ea7bf56789bfe93e1
yuanhao <994303>
2017-12-06 15:38:39 +0800

    branch-001：config.java中gexf的文件目录需要手动设置。branch-002：添加可视化显示功能。在数据管理模块从学科、课程、主题、分面等不同维度统计知识森林目前所有的数据分布情况

--- src/app/Config.java
+++ src/app/Config.java
@@ -23,6 +23,13 @@ public class Config {
 	/**
 	 * Mysql 配置
 	 */
+	public static String DBNAME = "yotta";
+	public static String HOST = "localhost";
+	public static String USERNAME = "root";
+	public static String PASSWD = "root";
+	public static int PORT = 9220;
+//	public static String MYSQL_URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DBNAME + 
+//			"?user=" + USERNAME + "&password=" + PASSWD + "&characterEncoding=UTF8&serverTimezone=UTC"; // 阿里云服务器：域名+http端口
 	public static String MYSQL_URL = "jdbc:mysql://localhost:9220/yotta?user=root&password=root&characterEncoding=UTF8"; // 阿里云服务器：域名+http端口
 	public static String IP1="http://yotta.xjtushilei.com:888"; // 跨域访问控制：域名+apache端口
 	public static String IP2="http://yotta.xjtushilei.com:9218/Yotta"; // 阿里云服务器：域名+http端口
@@ -34,6 +41,7 @@ public class Config {
 	public static ZHConverter converter = ZHConverter.getInstance(ZHConverter.SIMPLIFIED);// 转化为简体中文
 	public static String USER_INFO = "user_info";
 	public static String USER_LOG = "user_log";
+	public static String SUBJECT_TABLE = "subject";
 	public static String DOMAIN_TABLE = "domain";
 	public static String FACET_TABLE = "facet";
 	public static String FACET_RELATION_TABLE = "facet_relation";
@@ -53,5 +61,11 @@ public class Config {
 	
 	public static int CONTENTLENGTH = 0;
 	public static int DEPENDENCEMAX = 300;
+	
+	/**
+	 * 认知关系算法参数：gephi-tookit
+	 */
+	public static int FILTERDEGREE = 2;
+	public static String GEXFPATH = "C:\\gexfpath";
 	
 }
